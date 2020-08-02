bf6c3aadf3a55ba30725cadf01d10d08a497b375
Dimeji <afasina@nasdng.com>
2020-08-02 03:19:16 +0100

    last min change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -7,7 +7,7 @@ it('responds with details about the current user', async () => {
 
     const cookie = await global.signin();
 
-    const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send().expect(200)
+    const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send().expect(2000)
 
     expect(response.body.currentuser.email).toEqual('test@test.com')
 });
