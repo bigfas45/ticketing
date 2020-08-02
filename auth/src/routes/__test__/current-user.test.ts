831dfe5720df54e4a244f8abc9cc0ec559c182bf
Dimeji <afasina@nasdng.com>
2020-08-02 03:25:06 +0100

    fixed test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,20 +1,24 @@
 import request from 'supertest';
-import {app} from '../../app';
-import {response} from 'express';
+import { app } from '../../app';
+import { response } from 'express';
 
 it('responds with details about the current user', async () => {
+  const cookie = await global.signin();
 
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
 
-    const cookie = await global.signin();
-
-    const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send().expect(200)
-
-    expect(response.body.currentuser.email).toEqual('test@test.com')
+  expect(response.body.currentuser.email).toEqual('test@test.com');
 });
 
-
 it('responds with null if not authenticated', async () => {
-    const response = await request(app).get('/api/users/currentuser').send().expect(200);
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
 
-    expect(response.body.currentuser).toEqual(null);
-})
+  expect(response.body.currentuser).toEqual(null);
+});
